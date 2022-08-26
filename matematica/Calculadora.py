2853796c8eb2fe23050f093e7cf41a2540685ebe
gustavolp1 <glpachecocsa@gmail.com>
2022-08-26 08:08:48 -0300

    calculadora

--- matematica/Calculadora.py
+++ matematica/Calculadora.py
@@ -18,9 +18,19 @@ def multiplicacao(va: float, vb: float):
 def divisao(va: float, vb: float):
     ''' Função que retorna a divisão entre dois valores
     '''
-    return va / vb
+    return va / vb if vb != 0 else np.inf
 
 def media_lista_valores(v:list):
     ''' Função que retorna a média entre N valores
     '''
-    return np.mean(v)
+    v_temp = v
+    v = []
+
+    for valor in v_temp:
+        if isinstance(valor, (int, float)):
+            v.append(valor)
+    
+    if len(v) > 0:
+        return np.mean(v)
+    else:
+        return 0
--- test/test_operacoes.py
+++ test/test_operacoes.py
@@ -1,84 +1,36 @@
 import pytest
+import numpy as np
 from matematica.Calculadora import soma, sub, multiplicacao, divisao, media_lista_valores
 
+@pytest.mark.op_simples
+@pytest.mark.positivos
 def test_soma_dois_valores_positivos():
     v1 = 5.0
     v2 = 7.0
     soma(v1, v2)
     assert 12 == soma(v1, v2)
 
-def test_soma_dois_valores_negativos():
-    v1 = -5.0
-    v2 = -7.0
+@pytest.mark.op_simples
+def test_soma_um_valor_positivo_um_negativo():
+    v1 = 5.0
+    v2 = -1.0
     soma(v1, v2)
-    assert -12 == soma(v1, v2)
+    assert 4 == soma(v1, v2)
 
-def test_soma_dois_valores_positivos_e_negativos():
-    v1 = -5.0
-    v2 = 7.0
-    soma(v1, v2)
-    assert 2 == soma(v1, v2)
+@pytest.mark.op_complexas
+def test_media_lista_positivos():
+    assert 2.5 == media_lista_valores([1, 2, 3, 4])
 
-def test_subtracao_dois_valores_positivos():
+@pytest.mark.exercicio_1
+def test_divisao_por_zero():
     v1 = 5.0
-    v2 = 7.0
-    sub(v1, v2)
-    assert -2 == sub(v1, v2)
+    v2 = 0.0
+    assert np.inf == divisao(v1, v2)
 
-def test_subtracao_dois_valores_negativos():
-    v1 = -5.0
-    v2 = -7.0
-    sub(v1, v2)
-    assert 2 == sub(v1, v2)
+@pytest.mark.exercicio_1
+def test_media_valores_invalidos():
+    assert 2 == media_lista_valores([1, 2, 3, 'a'])
 
-def test_subtracao_dois_valores_positivos_e_negativos():
-    v1 = -5.0
-    v2 = 7.0
-    sub(v1, v2)
-    assert -12 == sub(v1, v2)
-
-def test_multiplicacao_dois_valores_positivos():
-    v1 = 5.0
-    v2 = 7.0
-    multiplicacao(v1, v2)
-    assert 35 == multiplicacao(v1, v2)
-
-def test_multiplicacao_dois_valores_negativos():
-    v1 = -5.0
-    v2 = -7.0
-    multiplicacao(v1, v2)
-    assert 35 == multiplicacao(v1, v2)
-
-def test_multiplicacao_dois_valores_positivos_e_negativos():
-    v1 = -5.0
-    v2 = 7.0
-    multiplicacao(v1, v2)
-    assert -35 == multiplicacao(v1, v2)
-
-def test_divisao_dois_valores_positivos():
-    v1 = 5.0
-    v2 = 7.0
-    divisao(v1, v2)
-    assert 0.7142857142857143 == divisao(v1, v2)
-
-def test_divisao_dois_valores_negativos():
-    v1 = -5.0
-    v2 = -7.0
-    divisao(v1, v2)
-    assert 0.7142857142857143 == divisao(v1, v2)
-
-def test_divisao_dois_valores_positivos_e_negativos():
-    v1 = -5.0
-    v2 = 7.0
-    divisao(v1, v2)
-    assert -0.7142857142857143 == divisao(v1, v2)
-
-def test_media_lista_valores_positivos():
-    v = [1, 2, 3, 4, 5]
-    media_lista_valores(v)
-    assert 3 == media_lista_valores(v)
-
-def test_media_lista_valores_negativos():
-    v = [-1, -2, -3, -4, -5]
-    media_lista_valores(v)
-    assert -3 == media_lista_valores(v)
+@pytest.mark.exercicio_1
+def test_media_lista_valores_vazia():
+    assert 0 == media_lista_valores([])
